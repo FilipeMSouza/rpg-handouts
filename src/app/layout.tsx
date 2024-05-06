7b46e9e46d81f10df631d34b9e00cd970c7a0954
Matheus Sousa <matheus.sousa@nubank.com.br>
2024-05-05 22:52:53 -0300

    adding the navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,36 @@
+'use client';
+
 import React from 'react';
 import StyledComponentsRegistry from '@/lib/registry';
 import { GlobalStyle } from './style/global';
 
 import { Poppins } from 'next/font/google';
+import { LoginPage, Navbar, Page, PageLink } from '@/app/style';
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
   style: ['normal', 'italic'],
 });
+
+const NavBarComponent = () => {
+  return (
+    <Navbar>
+      <Page>
+        <PageLink href={'/'}>Home</PageLink>
+      </Page>
+      <Page>
+        <PageLink href={'/new-char'}>Create new character</PageLink>
+      </Page>
+      <Page>
+        <PageLink href={'/database'}>Character database</PageLink>
+      </Page>
+      <LoginPage>
+        <PageLink href={'/login'}>Login</PageLink>
+      </LoginPage>
+    </Navbar>
+  );
+};
 
 export default function RootLayout({
   children,
@@ -19,6 +41,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={poppins.className}>
         <StyledComponentsRegistry>
+          <NavBarComponent />
           {children}
           <GlobalStyle />
         </StyledComponentsRegistry>
