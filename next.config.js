3df275b1a624d6223fb36e245055d66f87302957
h80r <h80rgbc@gmail.com>
2024-09-16 14:26:20 -0300

    ➕ Include supabase deps and .env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      }
+    ]
+  },
   compiler: {
     styledComponents: true,
   },
   env: {
     REALTIME_ENDPOINT: process.env.REALTIME_ENDPOINT,
     SCOPE: process.env.SCOPE,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
   },
 };
