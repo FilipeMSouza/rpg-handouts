aa5d303c2898f66515ac7fc98924461a16865f0c
h80r <h80rgbc@gmail.com>
2024-09-16 14:33:08 -0300

    ✨ Create supabase hook for all players base info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,20 @@
 'use client';
 import Image from 'next/image';
 
-import type { pjData } from '@/@types/pjData';
-import useRealtimeState from '@/hooks/useRealtimeState';
-
 import { Character, Name, PlayerLink, Wrapper } from './style';
+import usePlayersSummary from '@/hooks/supabase/usePlayersSummary';
 
 const Home = () => {
-  const [players] = useRealtimeState<pjData[]>();
+  const players = usePlayersSummary();
 
   return (
     <Wrapper>
-      {players?.map((pj, i) => {
+      {players.map(({ name, image, color, id }) => {
         return (
-          <PlayerLink key={pj.name} href={`/player/${i}/idle`}>
-            <Image src={pj.image} alt={pj.name} width={90} height={90} />
+          <PlayerLink key={id} href={`/player/${id}/idle`}>
+            <Image src={image} alt={name} width={90} height={90} />
             <Character>
-              <Name color={pj.color}>{pj.name}</Name>
+              <Name color={color}>{name}</Name>
             </Character>
           </PlayerLink>
         );
