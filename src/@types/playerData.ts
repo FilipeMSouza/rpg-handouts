55d1cab906f557854a404a0d255f927af163eee7
h80r <h80rgbc@gmail.com>
2024-09-16 14:28:29 -0300

    🧐 Update pjData interface name

---
+++ src/@types/playerData.ts
@@ -0,0 +1,13 @@
+export type PlayerData = {
+  id: string;
+  name: string;
+  life_max: number;
+  mana_max: number;
+  level: number;
+  color: string;
+  avatar: string;
+  profession: string;
+  armor_class: number;
+  mana_current: number;
+  life_current: number;
+};
